9974e54cddf0cd6d9061d653763d9e31cbbdc915
UnseenWizzard <nico@riedmann.dev>
2021-12-11 18:59:09 +0100

    feat: Solve 2021-12-11

---
+++ 2021-12-11/solution.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"os"
+	"log"
+	"riedmann.dev/aoc21/util"
+)
+
+func increment(octopi [][]int) {
+	for i := range(octopi) {
+		for j := range(octopi[i]) {
+			octopi[i][j] += 1
+		}
+	}
+}
+
+func flash(i int, j int, octopi [][]int, alreadyFlashed [][]bool) {
+	if alreadyFlashed[i][j] {
+		return
+	} 
+	alreadyFlashed[i][j] = true
+
+	height := len(octopi)
+	width := len(octopi[0])
+	//top neighbours
+	if i-1 >= 0 {
+		if j-1 >= 0 {
+			octopi[i-1][j-1] += 1
+			if octopi[i-1][j-1] > 9 {
+				flash(i-1, j-1, octopi, alreadyFlashed)
+			}
+		}
+		octopi[i-1][j] += 1
+		if octopi[i-1][j] > 9 {
+			flash(i-1, j, octopi, alreadyFlashed)
+		}
+		if j+1 < width {
+			octopi[i-1][j+1] += 1
+			if octopi[i-1][j+1] > 9 {
+				flash(i-1, j+1, octopi, alreadyFlashed)
+			}
+		}
+	}
+	//right
+	if j+1 < width {
+		octopi[i][j+1] += 1
+		if octopi[i][j+1] > 9 {
+			flash(i, j+1, octopi, alreadyFlashed)
+		}
+	}
+	//left
+	if j-1 >= 0 {
+		octopi[i][j-1] += 1
+		if octopi[i][j-1] > 9 {
+			flash(i, j-1, octopi, alreadyFlashed)
+		}
+	}
+	//bottom neighbours
+	if i+1 < height {
+		if j-1 >= 0 {
+			octopi[i+1][j-1] += 1
+			if octopi[i+1][j-1] > 9 {
+				flash(i+1, j-1, octopi, alreadyFlashed)
+			}
+		}
+		octopi[i+1][j] += 1
+		if octopi[i+1][j] > 9 {
+			flash(i+1, j, octopi, alreadyFlashed)
+		}
+		if j+1 < width {
+			octopi[i+1][j+1] += 1
+			if octopi[i+1][j+1] > 9 {
+				flash(i+1, j+1, octopi, alreadyFlashed)
+			}
+		}
+	}
+}
+
+func step(octopi [][]int) int {
+	increment(octopi)
+
+	flashed := make([][]bool, len(octopi))
+	for i := range(flashed) {
+		flashed[i] = make([]bool, len(octopi[0]))
+	}
+
+	for i := range(octopi) {
+		for j := range(octopi[i]) {
+			if octopi[i][j] > 9 {
+				flash(i,j,octopi, flashed)
+			}
+		}
+	}
+
+	count := 0
+	for i := range(flashed) {
+		for j := range(flashed[i]) { 
+			if flashed[i][j] {
+				octopi[i][j] = 0
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func calculateSolution(input []string, steps int) (flashed int) {
+    octopi := util.ParseIntMap(input)
+	for i := 0; i < steps; i++ {
+		flashed += step(octopi)
+	}
+    return 
+}
+
+func main() {
+    if len(os.Args) < 2 {
+        log.Fatal("Session Cookie needed as arg to get puzzle input!")
+    }
+	sessioncookie := os.Args[1]
+	input := util.GetPuzzleInput(day, sessioncookie)
+
+	println("Solution: ", calculateSolution(input, 100))
+}
+const day = "11"
--- util/input_parser.go
+++ util/input_parser.go
@@ -5,8 +5,21 @@ import (
 	"strings"
 )
 
+func ParseIntMap(input []string) [][]int {
+	out := make([][]int, len(input))
+	for i, l := range(input) {
+		out[i] = ParseIntListBy(l, "")
+
+	}
+	return out
+}
+
 func ParseIntList(input string) []int {
-	split := strings.Split(input, ",")
+	return ParseIntListBy(input, ",")
+}
+
+func ParseIntListBy(input string, sep string) []int {
+	split := strings.Split(input, sep)
 	out := make([]int, len(split))
 	for i := range split {
 		out[i], _ = strconv.Atoi(split[i])
